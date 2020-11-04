03b779b51b4faae902e75715195e492c2f416d0a
nwjbrandon <nwjbrandon@gmail.com>
2020-11-04 22:51:18 +0800

    add sever code

--- server/main.py
+++ server/main.py
@@ -2,16 +2,32 @@ import paho.mqtt.client as mqtt
 from time import sleep
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code: " + str(rc))
+    if rc == 0:
+        print("Connected")
+    else:
+        print("Failed to connect. Error code: %d." % rc)
 
-client = mqtt.Client()
-client.on_connect = on_connect
+USERID = "nwjbrandon"
+PASSWORD = "password"
 
-client.connect("192.168.50.247", 1883, 60)
-while True:
-    print("Waiting for 2 seconds.")
-    sleep(2)
-    print("Sending message.")
-    client.publish("test", "This is a test.")
+def setup(hostname):
+    client = mqtt.Client()
+    client.connect(hostname, 1883, 60)
+    client.username_pw_set(USERID, PASSWORD)
+    client.tls_set("/home/nwjbrandon/secrets/ca.pem", "/home/nwjbrandon/secrets/client.crt", "/home/nwjbrandon/secrets/client.key")
+    client.on_connect = on_connect
+    client.loop_start()
+    return client
+
+def main():
+    client = setup("192.168.50.247") # IP of raspi
+    while True:
+        print("Waiting for 2 seconds.")
+        sleep(2)
+        print("Sending message.")
+        client.publish("test", "This is a test.")
+
+if __name__ == '__main__':
+    main()
 
 
