abeb2f12cc1c099797652ba5e1bdce7ce0ede1d9
nwjbrandon <nwjbrandon@gmail.com>
2020-11-08 00:20:06 +0800

    training code

--- client/main.py
+++ client/main.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import time
+import json
 
 USERID = "nwjbrandon"
 PASSWORD = "password"
@@ -7,17 +9,35 @@ CA_PEM = f"/home/{USERNAME}/secrets/ca.pem"
 CLIENT_CRT = f"/home/{USERNAME}/secrets/client.crt"
 CLIENT_KEY = f"/home/{USERNAME}/secrets/client.key"
 BROKER_IP = "192.168.50.190" #"192.168.50.247" # IP of raspi
+STATUS = 1
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Successfully connected to broker")
-        client.subscribe("test")
+        client.subscribe("/train/status")
     else:
         print("Connection failed with code: %d" %rc)
 
 
+def train_model():
+    time.sleep(3)
+
+
 def on_message(client, userdata, msg):
-    print(msg.payload)
+    global STATUS
+    payload = json.loads(msg.payload)
+    print(payload)
+    if payload["train_status"] == 1:
+        try:
+            train_model()
+            print("Done")
+            payload = {"episode_status":1}
+            client.publish("/episode/status", json.dumps(payload))
+        except:
+            payload = {"episode_status":0}
+            client.publish("/episode/status", json.dumps(payload))
+    else:
+        STATUS = 0
 
 def setup(hostname):
     client = mqtt.Client()
@@ -32,7 +52,10 @@ def setup(hostname):
 def main():
     client = setup(BROKER_IP)
     while True: 
-        pass
+        if STATUS == 0:
+            print("Shutting down")
+            time.sleep(3)
+            break
 
 if __name__ == "__main__":
     main()
--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
-from time import sleep
+import json
+import time
 
 USERID = "nwjbrandon"
 PASSWORD = "password"
@@ -13,8 +14,17 @@ BROKER_IP = "192.168.50.190" #"192.168.50.247" # IP of raspi
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected")
+        client.subscribe("/episode/status")
     else:
         print("Failed to connect. Error code: %d." % rc)
+
+def on_message(client, userdata, msg):
+    payload = json.loads(msg.payload)
+    print(payload)
+    if payload["episode_status"] == 1:
+        print("Episode Done")
+    else:
+        print("Error in episode")
 
 def setup(hostname):
     client = mqtt.Client()
@@ -22,16 +32,27 @@ def setup(hostname):
     client.username_pw_set(USERID, PASSWORD)
     client.tls_set(CA_PEM, CLIENT_CRT, CLIENT_KEY)
     client.on_connect = on_connect
+    client.on_message = on_message
     client.loop_start()
     return client
 
 def main():
     client = setup(BROKER_IP) 
+    episode = 1
+    print("Connecting to broker")
+    time.sleep(3)
     while True:
-        print("Waiting for 2 seconds.")
-        sleep(2)
-        print("Sending message.")
-        client.publish("test", "This is a test.")
+        x = input(f"Start Episode {episode} [y/N]\n")
+        if x == "" or x == "y":
+            payload = {"train_status":1}
+            client.publish("/train/status", json.dumps(payload))
+            episode += 1
+        else:
+            payload = {"train_status": 0}
+            client.publish("/train/status", json.dumps(payload))
+            print("Shutting down")
+            time.sleep(3)
+            break
 
 if __name__ == '__main__':
     main()
