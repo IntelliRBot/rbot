6a1c1edc78f6c7ec215d85e0a773ce5ab476d0d5
nwjbrandon <nwjbrandon@gmail.com>
2020-11-07 23:46:47 +0800

    refactor motor

--- client/motor.py
+++ client/motor.py
@@ -1,50 +1,46 @@
 import RPi.GPIO as GPIO
-from time import sleep
 
-class DIRECTION:
-    FORWARD = 1
-    STOP = 0
-    BACKWARD = -1
+FORWARD = 1
+STOP = 0
+BACKWARD = -1
+
+Motor1A = 13
+Motor1B = 11
+Motor2A = 16
+Motor2B = 18
 
 class Motor:
     def __init__(self):
         GPIO.setmode(GPIO.BOARD)
-        
-        self.Motor1A = 13
-        self.Motor1B = 11
-
-        self.Motor2A = 16
-        self.Motor2B = 18
  
-        GPIO.setup(self.Motor1A, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(self.Motor1B, GPIO.OUT, initial=GPIO.LOW)
-
-        GPIO.setup(self.Motor2A, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(self.Motor2B, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(Motor1A, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(Motor1B, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(Motor2A, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(Motor2B, GPIO.OUT, initial=GPIO.LOW)
 
     def _forward(self):
         print("Forward")
-        GPIO.output(self.Motor1A,GPIO.HIGH)
-        GPIO.output(self.Motor1B,GPIO.LOW)
+        GPIO.output(Motor1A,GPIO.HIGH)
+        GPIO.output(Motor1B,GPIO.LOW)
 
-        GPIO.output(self.Motor2A,GPIO.LOW)
-        GPIO.output(self.Motor2B,GPIO.HIGH)
+        GPIO.output(Motor2A,GPIO.LOW)
+        GPIO.output(Motor2B,GPIO.HIGH)
 
     def _backward(self):
         print("Backward")
-        GPIO.output(self.Motor1A,GPIO.LOW)
-        GPIO.output(self.Motor1B,GPIO.HIGH)
+        GPIO.output(Motor1A,GPIO.LOW)
+        GPIO.output(Motor1B,GPIO.HIGH)
 
-        GPIO.output(self.Motor2A,GPIO.HIGH)
-        GPIO.output(self.Motor2B,GPIO.LOW)
+        GPIO.output(Motor2A,GPIO.HIGH)
+        GPIO.output(Motor2B,GPIO.LOW)
 
     def _stop(self):
         print("Stop")
-        GPIO.output(self.Motor1A,GPIO.LOW)
-        GPIO.output(self.Motor1B,GPIO.LOW)
+        GPIO.output(Motor1A,GPIO.LOW)
+        GPIO.output(Motor1B,GPIO.LOW)
 
-        GPIO.output(self.Motor2A,GPIO.LOW)
-        GPIO.output(self.Motor2B,GPIO.LOW)
+        GPIO.output(Motor2A,GPIO.LOW)
+        GPIO.output(Motor2B,GPIO.LOW)
 
     def cleanup(self):
         print("Clean up")
