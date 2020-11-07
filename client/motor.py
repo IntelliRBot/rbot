271a0e15aa32c3edf8c3105a6089b8e8e4be146f
nwjbrandon <nwjbrandon@gmail.com>
2020-11-07 23:31:45 +0800

    refactor motor

--- client/motor.py
+++ client/motor.py
@@ -1,36 +1,59 @@
 import RPi.GPIO as GPIO
 from time import sleep
+
+class DIRECTION:
+    FORWARD = 1
+    STOP = 0
+    BACKWARD = -1
+
+class Motor:
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        
+        self.Motor1A = 13
+        self.Motor1B = 11
+
+        self.Motor2A = 16
+        self.Motor2B = 18
  
-GPIO.setmode(GPIO.BOARD)
- 
-Motor1A = 13
-Motor1B = 11
-Motor2A = 16
-Motor2B = 18
- 
-GPIO.setup(Motor1A,GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Motor1B,GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Motor2A,GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(Motor2B,GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.Motor1A, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.Motor1B, GPIO.OUT, initial=GPIO.LOW)
 
-try:
-    print('Press CTRL+C to Interrupt')
-    while True:
-        print ("Forward")
-        GPIO.output(Motor1A,GPIO.HIGH)
-        GPIO.output(Motor1B,GPIO.LOW)
-        GPIO.output(Motor2A,GPIO.LOW)
-        GPIO.output(Motor2B,GPIO.HIGH)
-        sleep(2)
+        GPIO.setup(self.Motor2A, GPIO.OUT, initial=GPIO.LOW)
+        GPIO.setup(self.Motor2B, GPIO.OUT, initial=GPIO.LOW)
 
-        print ("Backward")
-        GPIO.output(Motor1A,GPIO.LOW)
-        GPIO.output(Motor1B,GPIO.HIGH)
-        GPIO.output(Motor2A,GPIO.HIGH)
-        GPIO.output(Motor2B,GPIO.LOW)
-        sleep(2)
+    def _forward(self):
+        print("Forward")
+        GPIO.output(self.Motor1A,GPIO.HIGH)
+        GPIO.output(self.Motor1B,GPIO.LOW)
 
-except KeyboardInterrupt:
-    print('Interrupt')
-    GPIO.cleanup()
+        GPIO.output(self.Motor2A,GPIO.LOW)
+        GPIO.output(self.Motor2B,GPIO.HIGH)
 
+    def _backward(self):
+        print("Backward")
+        GPIO.output(self.Motor1A,GPIO.LOW)
+        GPIO.output(self.Motor1B,GPIO.HIGH)
+
+        GPIO.output(self.Motor2A,GPIO.HIGH)
+        GPIO.output(self.Motor2B,GPIO.LOW)
+
+    def _stop(self):
+        print("Stop")
+        GPIO.output(self.Motor1A,GPIO.LOW)
+        GPIO.output(self.Motor1B,GPIO.LOW)
+
+        GPIO.output(self.Motor2A,GPIO.LOW)
+        GPIO.output(self.Motor2B,GPIO.LOW)
+
+    def cleanup(self):
+        print("Clean up")
+        GPIO.cleanup()
+    
+    def set_direction(self, direction):
+        if direction == DIRECTION.FORWARD:
+            self._forward()
+        if direction == DIRECTION.STOP:
+            self._stop()
+        if direction == DIRECTION.BACKWARD:
+            self._backward()
